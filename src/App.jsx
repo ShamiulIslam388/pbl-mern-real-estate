e205244d12fce31475d8980b84dda7dd1ba4134a
ShamiulIslam388 <shamiulislam388@gmail.com>
2023-11-26 12:52:08 +0600

    initial project setup with react vite and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="text-red-500">
+      This is react tailwind reacl estate project
+    </div>
+  );
+}
+
+export default App;
