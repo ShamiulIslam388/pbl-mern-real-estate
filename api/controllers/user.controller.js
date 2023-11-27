387711452fd2fa828e609d539dc486a8e013ae3f
ShamiulIslam388 <shamiulislam388@gmail.com>
2023-11-27 12:06:54 +0600

    update user api route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,5 +1,32 @@
-const updateUser = (req, res) => {
-  res.json(req.user);
+const { errorHandler } = require("../utils/error");
+const bcrypt = require("bcryptjs");
+const User = require("../models/user.model");
+
+const updateUser = async (req, res, next) => {
+  const { username, email, password, avatar } = req.body;
+  try {
+    if (req.user.id !== req.params.id)
+      return next(errorHandler(401, "Unauthorized user"));
+    if (password) {
+      password = bcrypt.hashSync(password, 10);
+    }
+    const updateduser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username,
+          email,
+          password,
+          avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password: pass, ...userWithoutPassword } = updateduser._doc;
+    return res.status(200).json(userWithoutPassword);
+  } catch (error) {
+    next(error);
+  }
 };
 
 module.exports = { updateUser };
