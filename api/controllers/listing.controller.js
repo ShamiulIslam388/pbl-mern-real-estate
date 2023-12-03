387528d102f86e7d8b5db5fd209feb3a49e11304
ShamiulIslam388 <shamiulislam388@gmail.com>
2023-12-03 22:38:31 +0600

    create deletelisting api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const Listing = require("../models/listing.model");
+const { errorHandler } = require("../utils/error");
 
 const createListing = async (req, res, next) => {
   try {
@@ -9,4 +10,17 @@ const createListing = async (req, res, next) => {
   }
 };
 
-module.exports = { createListing };
+const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+    if (!listing) return next(errorHandler(404, "No listing found"));
+    if (req.user.id !== listing.userRef)
+      return next(errorHandler(401, "Unauthorized user"));
+    await Listing.findByIdAndDelete(req.params.id);
+    res.status(200).json({ message: "listing is deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing };
